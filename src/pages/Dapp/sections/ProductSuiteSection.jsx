fdf56dd9115f4e415e3da3131b4f39df15a50720
akashmadduru <akashmadduru@gmail.com>
2024-02-14 20:52:15 +0530

    Final Changes

--- src/pages/Dapp/sections/ProductSuiteSection.jsx
+++ src/pages/Dapp/sections/ProductSuiteSection.jsx
@@ -27,13 +27,9 @@ export default function ProductSuiteSection() {
                 borderRadius: "lg",
                 p: 4,
                 className: "glass_effect",
-                bg: "gray.50",
-                backgroundImage: StakingImages[i],
-                backgroundSize: "contain",
-                backgroundPosition: "bottom",
-                backgroundRepeat: "no-repeat",
+                bg: c.bg,
                 h: "100%",
-                bg: "black",
+                bgImg: c.bgImg,
                 display: "flex",
                 flexDir: "column",
                 justifyContent: "space-between",
@@ -42,15 +38,25 @@ export default function ProductSuiteSection() {
                 boxShadow: "sm",
               }}
             >
+              <Box
+                zIndex={3}
+                backgroundImage={StakingImages[i]}
+                backgroundSize="contain"
+                backgroundPosition="bottom"
+                backgroundRepeat="no-repeat"
+                minH="40rem"
+                pos="absolute"
+                w="100%"
+              ></Box>
               <Grid mt={2} rowGap={"1rem"}>
                 <CText
-                  cprops={{ color: "gray.300" }}
+                  cprops={{ color: "gray.100" }}
                   size={1}
                   title={c.title}
                 />
-                <CText cprops={{ color: "gray.500" }} size={3} title={c.desc} />
+                <CText cprops={{ color: "gray.200" }} size={3} title={c.desc} />
               </Grid>
-              <Button onClick={() => navigate(c.link)} mt={6}>
+              <Button zIndex={4} onClick={() => navigate(c.link)} mt={6}>
                 {c.btnText}
               </Button>
             </CCard>
@@ -63,20 +69,20 @@ export default function ProductSuiteSection() {
 
 const data = [
   {
-    bg: "#FFECD9",
+    bgImg: "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
     title: "AI Art Generator",
     desc: "Dive into the realm of artistic exploration with our advanced AI Art Generator. We've created a unique platform that allows you to unleash your creativity. We also enable users to easily mint and inscribe your unique creations as BRC-721 tokens instantly on the Bitcoin blockchain.",
     btnText: "Try Now",
     link: "/dapp/image-generation",
   },
   {
-    bg: "#DBFFD9",
+    bgImg: "linear-gradient(to right, #c31432, #240b36);",
     title: "AI Trading Bot",
     desc: "Seize trading opportunities like never before with our AI Trading Bot. We provide AI-driven insights and trends acorss all the coins in top exchanges. Use our intuitive telegram bot to seamlessly trade and maximise profits.",
     btnText: "Get Plan",
   },
   {
-    bg: "#BCFDC3",
+    bgImg: "linear-gradient(to right, #41295a, #2f0743);",
     title: "GPT extensions and plugins",
     desc: "Discover our suite of extensions and plugins designed to unlock the full potential of GPT in your endeavors. We provide tools tailored specifically for web3 users to harness the power of GPT technology on the BRC20 ecosystem.",
     btnText: "Coming Soon",
